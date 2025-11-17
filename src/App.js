67d882a113a28b48fb62f16b17b859b9a17846c1
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-11-17 21:51:32 +0300

    THIS IS A CHECKPOINT COMMIT! START MAKING AN INTEGRATION WITH NEW MAXIM'S DESIGN. 1.29. CRITICAL UPDATE (GLOBAL STYLES)

--- src/App.js
+++ src/App.js
@@ -57,11 +57,9 @@ const Layout = ({ children }) => {
     
     if (useSidebar) {
         return (
-            <div style={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+            <div className="app">
                 <Sidebar />
-                <div style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
-                    {children}
-                </div>
+                {children}
             </div>
         );
     }
