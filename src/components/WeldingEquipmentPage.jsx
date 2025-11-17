8f7eb692d9c0fcee712653bc76b872f882f0b66e
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-11-17 12:12:54 +0300

    THIS IS A CHECKPOINT COMMIT! START MAKING AN INTEGRATION WITH NEW MAXIM'S DESIGN. 1.10. Added table format for WeldingEquipmentPage.jsx

--- src/components/WeldingEquipmentPage.jsx
+++ src/components/WeldingEquipmentPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/weldingEquipmentPageNew.css';
+import '../styles/weldingEquipmentTable.css';
 import { useNavigate } from 'react-router-dom';
 import AddEquipmentModal from './AddEquipmentModal';
 import machineImage from '../images/Untitled 3 копия.png';
