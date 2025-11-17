3e396e30be0b3781abc5a19da326718cc1179627
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-11-17 21:40:50 +0300

    THIS IS A CHECKPOINT COMMIT! START MAKING AN INTEGRATION WITH NEW MAXIM'S DESIGN. 1.28. CRITICAL UPDATE (GLOBAL STYLES)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main_page.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
